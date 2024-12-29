98735fdcb6032cbd79c210a6e9e020e430633b1d
RHYTHM BHARDWAJ <rhythmiscoding@gmail.com>
2024-12-30 02:55:42 +0530

    Updated copyright information in jbh-checker.js
    Signed-off-by: RHYTHM BHARDWAJ <rhythmiscoding@gmail.com>

--- jbh-checker.js
+++ jbh-checker.js
@@ -1,4 +1,20 @@
-// Hosting the script externally and serving it via HTTPS.
+/**
+* Copyright (c) 2024, Rhytmelo Designs Agency. All rights reserved.
+*
+* This software is proprietary to Rhytmelo Designs Agency. No rights are granted to use, 
+* download, copy, modify, distribute, or otherwise exploit this software in any form, 
+* except as expressly authorized in writing by Rhytmelo Designs Agency.
+*
+* Unauthorized use of this software or any part thereof is strictly prohibited. Any 
+* violations may result in legal action under applicable laws.
+*
+* THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+* PURPOSE, OR NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+* FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT, OR 
+* OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*/
 
   document.addEventListener('DOMContentLoaded', () => {
     const checkerId = 'flexi-checker';
